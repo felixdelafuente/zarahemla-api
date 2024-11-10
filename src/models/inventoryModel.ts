64bccd51c11c440b19cda8e1de6da73e9011a17a
Felix Dela Fuente <felixjerome.delafuente@gmail.com>
2024-11-10 21:00:13 +0800

    chore: change inventory and service model

--- src/models/inventoryModel.ts
+++ src/models/inventoryModel.ts
@@ -3,29 +3,23 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 // Define the schema for the inventory items
 interface IInventoryItem extends Document {
-  brand: string;
-  name: string;
-  description: string;
   category: string;
   size: string;
+  brand: string;
+  description: string;  
   quantity: string;
   unit: string;
-  dateIssued: Date;
-  costPrice: number;
   sellingPrice: number;
 }
 
 // Create the schema with properties
 const inventorySchema = new Schema<IInventoryItem>({
-  brand: { type: String, required: true },
-  name: { type: String, required: true },
-  description: { type: String },
   category: { type: String, required: true },
   size: { type: String, required: true },
+  brand: { type: String, required: true },
+  description: { type: String },
   quantity: { type: String, required: true },
   unit: { type: String, required: true },
-  dateIssued: { type: Date, default: Date.now },
-  costPrice: { type: Number, required: true },
   sellingPrice: { type: Number, required: true },
 });
 
--- src/models/serviceModel.ts
+++ src/models/serviceModel.ts
@@ -2,22 +2,22 @@ import mongoose, { Document, Schema, Types } from 'mongoose';
 
 // Define the interface for Service Document
 export interface IService extends Document {
+  category: string;
   name: string;
   description: string;
   duration: string;
   frequency: string;
-  sellingPrice: number;
-  parts: Types.ObjectId[];  // Array of Inventory item ObjectIds
+  price: number;
 }
 
 // Define the schema for Service collection
 const ServiceSchema: Schema = new Schema({
+  category: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: String, required: true },
   frequency: { type: String, required: true },
-  sellingPrice: { type: Number, required: true },
-  parts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Inventory', required: true }]
+  price: { type: Number, required: true }
 });
 
 export default mongoose.model<IService>('Service', ServiceSchema);
